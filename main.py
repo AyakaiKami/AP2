602c749aa670cdfcc9cec5e013e7c7f385c0d550
NAIex <alexneagubiz@gmail.com>
2025-01-17 19:45:53 +0200

    Regression Tree

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils import data_parser
 
 from algs import linear_regression
 
-from sklearn import linear_model
+from sklearn import linear_model, tree
 
 import numpy as np
 
@@ -12,7 +12,8 @@ file_path="Data/tourism_dataset.csv"
 GET_INPUT = False
 
 LINEAR_REGRESSION = 0
-ALGORITHM_USED = LINEAR_REGRESSION
+REGRESSION_TREE = 1
+ALGORITHM_USED = REGRESSION_TREE
 
 
 whole_data = data_parser.load_data(file_path)
@@ -77,7 +78,53 @@ if ALGORITHM_USED in {LINEAR_REGRESSION}:
     for x, y in zip(ranking_pred,ranking_real):
         print(x,y)        
         #print(f'Category: {key} | Pred: {categoryPredictions[key]}| Real:{categoryReals[key]}')
+elif ALGORITHM_USED == REGRESSION_TREE:
+    data_parser.convert_to_numeric(whole_data)
+    td, vd = data_parser.split_train_validation(whole_data,0.9)
 
+    # Training data
+    a_1, t_1 = data_parser.split_attrib_target(td,3)
+    a_1 = np.array(a_1,dtype=np.float64)
+    t_1 = np.array(t_1,dtype=np.float64)
+
+    rt = tree.DecisionTreeRegressor()
+    rt = rt.fit(a_1,t_1)
+
+    # Testing data
+    a_2, t_2 = data_parser.split_attrib_target(vd,3)
+    a_2 = np.array(a_2,dtype=np.float64)
+    t_2 = np.array(t_2,dtype=np.float64)
+
+    predictions = rt.predict(a_2)
+    #predictions = reg2.predict(a_2)
+    real        = t_2
+
+    categoryPredictions = {}
+    categoryReals       = {}
+
+    categoryPredictions = {}
+    categoryReals       = {}
+
+    for vals, p, r in zip(a_2,predictions,real):
+        type = vals[0]
+        if categoryPredictions.get(type) == None:
+            categoryPredictions[type] = p
+            categoryReals[type]       = r
+        else:
+            categoryPredictions[type] += p
+            categoryReals[type]       += r
+
+
+    final_ranking = []
+    for key in categoryPredictions.keys():
+        final_ranking.append([key,categoryPredictions[key],categoryReals[key]])
+
+    ranking_pred = sorted(final_ranking,key=lambda x:x[1],reverse=True)
+    ranking_real = sorted(final_ranking,key=lambda x:x[2],reverse=True)
+    for x, y in zip(ranking_pred,ranking_real):
+        print(x[0],y[0])        
+
+    pass
     
 
 
